20a9a2bc9433f9427616e5e36480ad907ce1c20d
Pierrick Roger <pk.roger@icloud.com>
2022-03-03 15:25:52 +0100

    Add long tests.

---
+++ longtests/testthat.R
@@ -0,0 +1,7 @@
+# Script needed to run testthat automatically from ‘R CMD check’. See
+# testthat::test_dir documentation.
+library(testthat)
+library(biodbNci)
+Sys.setenv(TESTTHAT_REPORTER="summary")
+Sys.setenv(BIODB_LOG_DST="console")
+test_check("biodbNcbi")
